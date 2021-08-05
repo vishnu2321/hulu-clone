741bb4467ec963cb041e22f0b88b27437d8579fa
vishnu2321 <mandalavishnu750@gmail.com>
2021-08-05 20:50:56 +0530

    changed-display-name-for-thumbnal

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import {ThumbUpIcon} from '@heroicons/react/outline';
 import { forwardRef } from 'react';
 
-const  Thumbnail= forwardRef(({result},ref)=> {
+const Thumbnail= forwardRef(({result},ref)=> {
     const BASE_URL = 'https://image.tmdb.org/t/p/original'
     return (
         <div className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
@@ -32,4 +32,6 @@ const  Thumbnail= forwardRef(({result},ref)=> {
     )
 })
 
-export default Thumbnail
+Thumbnail.displayName="Thumbnail";
+
+export default Thumbnail;
